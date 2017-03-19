4dc5141750679972ca403378e1cf7d5a1242881e
Muhammad Azizul Hakim <19azizul92@gmail.com>
2017-03-19 10:05:26 +0700

    Create PythonScript.py

---
+++ Lab_1-Getting Started with Azure Machine Learning/PythonScript.py
@@ -0,0 +1,5 @@
+#Execute Python Script
+def azureml_main(dataframe1 = None, dataframe2 = None):
+## delete yr and weathersit columns
+    dataframe1.drop(['yr', 'weathersit'], 1, inplace = True)
+    return dataframe1
